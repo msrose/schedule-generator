dd546888932c475a798b1c5b9efabed74459b51a
Michael Rose <ekimsc1094@sympatico.ca>
2014-04-27 21:18:18 -0400

    Miscellaneous refactoring.

--- waterloo/schedule.rb
+++ waterloo/schedule.rb
@@ -6,30 +6,31 @@ module Waterloo
     MINUTES_PER_SLOT = 30
     DAY_START_TIME = '8:30'
     TIME_FORMAT = '%d:%02d'
+    TEMPLATE_PATH = './waterloo/template.rhtml'
 
     attr_reader :courses
     attr_accessor :title, :term, :orientation, :legends, :colors
 
     def initialize(courses = [])
-      @courses = courses
+      @courses = courses.uniq
       @orientation = :horizontal
       @legends = [:course_name, :instructor]
       @colors = ['white']
       yield self if block_given?
 
-      interval_size = time_interval_size(DAY_START_TIME, calculate_day_end)
       @day_start_minute_val = minute_value(DAY_START_TIME)
+      interval_size = time_interval_size(DAY_START_TIME, calculate_day_end)
       @grid = Hash[DAYS.zip(Array.new(DAYS.length) { Array.new(interval_size) })]
       initialize_time_markers(interval_size)
       populate_grid
     end
 
     def add(course)
-      @courses.push(course)
+      @courses.push(course) unless @courses.include?(course)
     end
 
     def generate
-      puts ERB.new(File.open('./waterloo/template.rhtml').read).result(binding).gsub(/^\s+/, '')
+      puts ERB.new(File.open(TEMPLATE_PATH).read).result(binding).gsub(/^\s+/, '')
     end
 
     private
@@ -43,14 +44,19 @@ module Waterloo
               [days].flatten.each do |day|
                 day_symbol = day.to_sym
                 for i in first_slot...last_slot
-                  if @grid[day_symbol][i].nil?
+                  current_grid_value = @grid[day_symbol][i]
+                  if current_grid_value.nil?
+                    location = time_slot.locations[day] || time_slot.locations['default']
                     @grid[day_symbol][i] = {
                       id: time_slot.id,
-                      text: course.code + '<br>' + time_slot.section + '<br>' + time_slot.locations.first,
+                      text: "#{course.code}<br>#{time_slot.section}<br>#{location}",
                       color: @colors[k % @colors.count]
                     }
                   else
-                    raise "Time conflict for #{course.code} from #{start_time} - #{end_time}"
+                    conflict_datetime = "Time conflict on #{day.capitalize} from #{start_time}-#{end_time}"
+                    current_slot = current_grid_value[:text].split('<br>')[0..1].join(' ')
+                    conflict_slot = "#{course.code} #{time_slot.section}"
+                    raise "#{conflict_datetime}: \"#{conflict_slot}\" and \"#{current_slot}\""
                   end
                 end
               end
@@ -69,35 +75,30 @@ module Waterloo
       end
 
       def calculate_day_end
-        times = @courses.collect(&:time_slots).flatten.collect(&:meeting_info).map(&:values).flatten
+        times = @courses.collect(&:time_slots).flatten.collect(&:meeting_info).collect(&:values).flatten
         times.sort do |a, b|
           minute_value(a) <=> minute_value(b)
         end.last
       end
 
       def initialize_time_markers(interval_size)
-        @time_markers = (0...interval_size).to_a.map do |el|
-          min_val = @day_start_minute_val + MINUTES_PER_SLOT * el
+        @time_markers = (0...interval_size).to_a.map do |n|
+          min_val = @day_start_minute_val + MINUTES_PER_SLOT * n
           TIME_FORMAT % min_val.divmod(60)
         end
       end
 
-      def calculate_span_value(id, index, time_slots)
+      def calculate_span_value(time_slots, index)
         span = 1
-        while (index += 1) < time_slots.length
-          break unless time_slots[index] && time_slots[index][:id] == id
-          span += 1
-        end
+        return span if time_slots[index].nil?
+        id = time_slots[index][:id]
+        span += 1 while (index += 1) < time_slots.length && !time_slots[index].nil? && time_slots[index][:id] == id
         span
       end
 
       def twelve_hour_time(time)
         hour, minute = time.split(':').map(&:to_i)
-        if hour <= 12
-          time
-        else
-          TIME_FORMAT % [hour - 12, minute]
-        end
+        hour <= 12 ? time : TIME_FORMAT % [hour - 12, minute]
       end
   end
 end
